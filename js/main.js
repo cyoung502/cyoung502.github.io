cbbc23b67bd14d1a88ca3787f2801312213a7630
Zobris <zobris@gmail.com>
2018-11-15 11:55:54 -0500

    fix
    fixed

---
+++ js/main.js
@@ -0,0 +1,78 @@
+var w;
+let city, weatherData;
+
+function getCity(callback){
+	$.ajax({
+		url: "http://ip-api.com/json",
+		dataType: "jsonp",
+		cache: false,
+		jsonpCallback: "callbackFunction",
+		success: function(result){
+			city = result.city + ", " + result.region;
+			let weatherUrl = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')&format=json";
+			callback(weatherUrl);
+		}
+	});
+
+}
+
+function getWeatherData(weatherUrl){
+	$.ajax({
+		url: weatherUrl,
+		dataType: 'jsonp',
+		cache: false,
+		success: function(result){
+			weatherData = result.query.results.channel;
+			w = new Weather(city, weatherData);
+			w.init();
+			setToday();
+			setForecast();
+		}
+	});
+}
+
+function setToday(){
+	document.getElementById("sunrise").innerHTML = w.weatherData.astronomy.sunrise;
+	document.getElementById("sunset").innerHTML = w.weatherData.astronomy.sunset;
+	document.getElementById("humidity").innerHTML = w.weatherData.atmosphere.humidity + " " + "%";
+	document.getElementById("pressure").innerHTML = w.weatherData.atmosphere.pressure + " " + w.weatherData.units.pressure;
+	document.getElementById("visibility").innerHTML = w.weatherData.atmosphere.visibility + " " + w.weatherData.units.distance;
+	document.getElementById("temperature").innerHTML = w.weatherData.item.condition.temp + " " + w.weatherData.units.temperature;
+	document.getElementById("condition").innerHTML = w.weatherData.item.condition.text;
+	document.getElementById("chill").innerHTML = w.weatherData.wind.chill + " " + w.weatherData.units.temperature;
+	document.getElementById("direction").innerHTML = w.weatherData.wind.direction;
+	document.getElementById("speed").innerHTML = w.weatherData.wind.speed + " " + w.weatherData.units.speed;
+	document.getElementById("latitude").innerHTML = w.weatherData.item.lat + "&deg;";
+	document.getElementById("longitude").innerHTML = w.weatherData.item.long + "&deg;";
+	document.getElementById("date").innerHTML = w.weatherData.item.forecast[0].day + ", " + w.weatherData.item.forecast[0].date;
+	document.getElementById("location").innerHTML = w.city;	
+}
+
+function setForecast(){
+	let table = document.getElementById("forecast");
+	let thead = table.appendChild(document.createElement("thead"));
+	let tr = thead.appendChild(document.createElement("tr"));
+	for(let i = 0; i < 10; i++){
+		let td = document.createElement("td");
+		td.innerHTML = w.weatherData.item.forecast[i].day;
+		tr.appendChild(td);
+	}
+	let tbody = table.appendChild(document.createElement("tbody"));
+	tr = tbody.appendChild(document.createElement("tr"));
+	for(let i = 0; i < 10; i++){
+		let td = document.createElement("td");
+		td.innerHTML = "<div class=\"small\">" + w.weatherData.item.forecast[i].text + "</div>" + "<br>"
+		+ "High: " + w.weatherData.item.forecast[i].high + "<br>"
+		+ "Low: " + w.weatherData.item.forecast[i].low;
+		tr.appendChild(td);
+	}
+}
+
+function getData(){
+	getCity(getWeatherData);
+}
+
+
+window.onload = function(){
+	getData();
+}
---
+++ js/weather.js
@@ -0,0 +1,170 @@
+let imageExtension = ".jpg";
+
+function Weather(city, weatherData){
+	this.city = city;
+	this.weatherData = weatherData;
+	this.background = "images/bg/";
+	this.status = "images/status/";
+}
+
+Weather.prototype.setBackground = function(image){
+	this.background = this.background + image + imageExtension;
+}
+
+Weather.prototype.setStatus = function(image){
+	this.status = this.status + image + imageExtension;
+}
+
+Weather.prototype.setImages = function(image){
+	this.setBackground(image);
+	this.setStatus(image);
+}
+
+Weather.prototype.init = function(){
+	let weatherCode = this.weatherData.item.condition.code;
+	switch(weatherCode) {
+		case "0":
+			this.setImages("tornado");
+			break;
+		case "1":
+			this.setImages("tropical_storm");
+			break;
+		case "2":
+			this.setImages("hurricane");
+			break;
+		case "3":
+			this.setImages("severe_thunderstorms");
+			break;
+		case "4":
+			this.setImages("thunderstorms");
+			break;
+		case "5":
+			this.setImages("mixed_rain_and_snow");
+			break;
+		case "6":
+			this.setImages("mixed_rain_and_sleet");
+			break;
+		case "7":
+			this.setImages("mixed_snow_and_sleet");
+			break;
+		case "8":
+			this.setImages("freezing_drizzle");
+			break;
+		case "9":
+			this.setImages("drizzle");
+			break;
+		case "10":
+			this.setImages("freezing_rain");
+			break;
+		case "11":
+		case "12":
+			this.setImages("showers");
+			break;
+		case "13":
+			this.setImages("snow_flurries");
+			break;
+		case "14":
+			this.setImages("light_snow_showers");
+			break;
+		case "15":
+			this.setImages("blowing_snow");
+			break;
+		case "16":
+			this.setImages("snow");
+			break;
+		case "17":
+			this.setImages("hail");
+			break;
+		case "18":
+			this.setImages("sleet");
+			break;
+		case "19":
+			this.setImages("dust");
+			break;
+		case "20":
+			this.setImages("foggy");
+			break;
+		case "21":
+			this.setImages("haze");
+			break;
+		case "22":
+			this.setImages("smoky");
+			break;
+		case "23":
+			this.setImages("blustery");
+			break;
+		case "24":
+			this.setImages("windy");
+			break;
+		case "25":
+			this.setImages("cold");
+			break;
+		case "26":
+			this.setImages("cloudy");
+			break;
+		case "27":
+			this.setImages("mostly_cloudy_night");
+			break;
+		case "28":
+			this.setImages("mostly_cloudy_day");
+			break;
+		case "29":
+			this.setImages("partly_cloudy_night");
+			break;
+		case "30":
+			this.setImages("partly_cloudy_day");
+			break;
+		case "31":
+			this.setImages("clear_night");
+			break;
+		case "32":
+			this.setImages("sunny");
+			break;
+		case "33":
+			this.setImages("fair_night");
+			break;
+		case "34":
+			this.setImages("fair_day");
+			break;
+		case "35":
+			this.setImages("mixed_rain_and_hail");
+			break;
+		case "36":
+			this.setImages("hot");
+			break;
+		case "37":
+			this.setImages("isolated_thunderstorms");
+			break;
+		case "38":
+		case "39":
+			this.setImages("scattered_thunderstorms");
+			break;
+		case "40":
+			this.setImages("scattered_showers");
+			break;
+		case "41":
+			this.setImages("heavy_snow");
+			break;
+		case "42":
+			this.setImages("scattered_snow_showers");
+			break;
+		case "43":
+			this.setImages("heavy_snow");
+			break;
+		case "44":
+			this.setImages("partly_cloudy");
+			break;
+		case "45":
+			this.setImages("thundershowers");
+			break;
+		case "46":
+			this.setImages("snow_showers");
+			break;
+		case "47":
+			this.setImages("isolated_thundershowers");
+			break;
+		case "3200":
+			this.setImages("reeee");
+			break;
+	}
+}
